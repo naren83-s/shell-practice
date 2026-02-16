174b5d1ce01a8d2d9e5dad32f362030c08ae6ccf
narendra <narendra.sunkara83@gmail.com>
2026-02-16 21:26:40 +0400

    action

---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=suresh
+PERSON2=ramesh
+echo "$PERSON1:: hello $PERSON2 how are you"
+echo "$PERSON2:: hi $PERSON1, i am fine .thanks for asking"
+echo "$PERSON1:: doing fine. what are you learning $PERSON2"
+echo "$PERSON2:: i am learning shell. what about you"
