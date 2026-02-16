edea818ddf595db8f2e41a137bea726805f5bf2c
narendra <narendra.sunkara83@gmail.com>
2026-02-16 20:33:44 +0400

    action

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "suresh:: hello rsmesh how are you"
+echo "ramesh:: hi suresh, i am fine .thanks for asking"
+echo "suresh:: doing fine. what are you learning ramesh"
+echo " ramesh:: i am learning shell. what about you"
