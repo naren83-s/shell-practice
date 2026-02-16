978118150fcc029a055015c5f291ee5e88db19c2
narendra <narendra.sunkara83@gmail.com>
2026-02-16 22:06:02 +0400

    action

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-START_TIME=$(date)
+START_TIME=$(date +%s)
 echo "script executed at:: $START_TIME"
 
 sleep 10
 
-END_TIME=$(date)
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($NED_TIME-$START_TIME))
+
+echo "scipt executed in : $TOTAL_TIME"
