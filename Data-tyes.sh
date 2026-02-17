413bbe48e428ab478ab973982379ea0c127a3991
narendra <narendra.sunkara83@gmail.com>
2026-02-17 15:53:10 +0400

    action

--- Data-tyes.sh
+++ Data-tyes.sh
@@ -5,3 +5,10 @@ NUM2=200
 
 SUM=$((NUM1+NUM2))
 echo " sum is : $SUM"
+
+FRUITS=("apple" "banana" "pomo")
+
+echo "fruits are ${FRUITS[@]}"
+echo " FIrst Fruit is : ${FRUITS[0]}"
+echo " Second Fruit is : ${FRUITS[1]}"
+echo " Thitd Fruit is : ${FRUITS[2]}"
