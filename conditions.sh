7efec42785b52ad45b7f4fabe60dc815f0c76409
narendra <narendra.sunkara83@gmail.com>
2026-02-17 16:25:09 +0400

    action

--- conditions.sh
+++ conditions.sh
@@ -4,6 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then
     echo " Given number : $NUMBER is greater then 20"
+elif [ $NUMBER -eq 20 ]; then
+    echo " Given number : $NUMBER is equal to 20"
 else 
     echo " Given number : $NUMBER is less then 20"
 fi
