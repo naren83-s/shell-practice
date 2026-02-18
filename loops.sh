738ff8c45e1873654ceef758a154779e9157f42a
narendra <narendra.sunkara83@gmail.com>
2026-02-18 18:09:09 +0400

    action

---
+++ loops.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOG_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+   echo " Please run this script with root user access" | tee -a $LOG_FILE
+   exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]; then
+       echo "$2 ....... FAILURE" | tee -a $LOG_FILE
+       exit 1
+    else
+       echo "$2 ........ SUCESS" | tee -a $LOG_FILE
+}
+
+for package in $@ 
+do 
+  dnf install $package -y
+  VALIDATE $? " Installing $package"
+done
