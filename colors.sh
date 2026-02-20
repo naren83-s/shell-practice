6b7c9755b78ed3be361ca3e354cb877add8cd781
narendra <narendra.sunkara83@gmail.com>
2026-02-20 15:23:26 +0400

    action

--- colors.sh
+++ colors.sh
@@ -1,37 +1,38 @@
+
 #!/bin/bash
 
 USERID=$(id -u)
-LOGS_FOLDER="var/log/shell-script"
-LOG_FILE="var/log/shell-script/$0.log"
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
 if [ $USERID -ne 0 ]; then
-   echo -e "$R Please run the scipt in root user$n" | tee -a $LOG_FILE
-   exit 1
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
 fi
 
 mkdir -p $LOGS_FOLDER
- VALIDATE (){
 
+VALIDATE(){
     if [ $1 -ne 0 ]; then
-       echo -e "$2 ...... $R FAILURE $N" | tee -a $LOG_FILE
-       exit 1
-    else 
-        echo -e "$2 ........ $G SUCESS $N" |tee -a $LOG_FILE
-    fi
- }
-
- for package in $@
- do 
-  dnf list installed $package &>> $LOG_FILE
-   if [ $? -ne 0 ]; then
-      echo -e "$package  Not  installed , $Y installing Now $N" 
-      dnf install $package &>> $LOG_FILE
-      VALIDATE $? "$package installing"
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
     else
-      echo -e "$packege $Y already installed $N" 
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+do
+    dnf list installed $package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not installed, installing now"
+        dnf install $package -y &>>$LOGS_FILE
+        VALIDATE $? "$package installation"
+    else
+        echo -e "$package already installed ... $Y SKIPPING $N"
     fi
 done
